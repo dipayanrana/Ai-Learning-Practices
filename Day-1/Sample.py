e3e42a4f0bdc76ccb34d594f379d8de5776ea24c
Dipayan Rana <dipayan@Dipayans-MacBook-Air.local>
2025-02-24 00:54:25 +0530

    Day 1: Python basics and Flask app

---
+++ Day-1/Sample.py
@@ -0,0 +1,16 @@
+#Arithmetic Operations
+a = 10
+b = 20
+c = a + b   # Addition
+d = a - b   # Subtraction
+e = a * b   # Multiplication
+f = a / b   # Division
+g = a % b   # Modulus
+h = a ** b  # Exponentiation
+i = a // b  # Floor Division
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)                    
